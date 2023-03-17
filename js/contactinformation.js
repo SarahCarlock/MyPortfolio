97a3b11b868dc1ff581b65f4f7b383d0d3d56237
Sarah Carlock <sarah.carlock86@gmail.com>
2023-03-17 12:42:54 -0500

    oops, a bunch of changes. sorry

---
+++ js/contactinformation.js
@@ -0,0 +1,11 @@
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "12.5rem";
+    document.getElementById("contact").style.marginLeft = "12.5rem";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("contact").style.marginLeft = "0";
+  }
