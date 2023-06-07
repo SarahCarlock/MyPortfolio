aacf8f26a79a9b36d5b9b46943f5920464b82808
Sarah Carlock <sarah.carlock86@gmail.com>
2023-06-07 18:19:35 -0500

    first implimentation of JS in portfolio website.

---
+++ JS-practice/scripts/currentDayDateTime.js
@@ -0,0 +1,30 @@
+const clockDisplay = document.getElementById('clockDisplay');
+const calendarDisplay = document.getElementById('calendarDisplay');
+const dateDisplay = document.getElementById('dateDisplay');
+
+function currentDay() {
+    let day = new Date().getDay();
+    let daylist = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    calendarDisplay.innerHTML = `Today is ${daylist[day]}.`;
+};
+
+function currentTime() {
+    const date = new Date(Date.now());
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
+
+    clockDisplay.innerHTML = `The current time is ${hour}:${minute}:${second}`;
+};
+
+function currentDate() {
+    const date = new Date(Date.now());
+
+    dateDisplay.innerHTML = date;
+};
+
+currentTime();
+currentDay();
+currentDate();
+
